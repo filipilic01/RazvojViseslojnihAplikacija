05a97a290d4f3e6df6b113fefaaee4ca72a676be
it7-2020 <it7-2020@AR1-10.ftn.ns>
2023-02-27 14:45:20 +0100

    Kreiran je inicijalni projekat

---
+++ Bolnica/src/main/java/rva/BolnicaApplication.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolnicaApplication.class, args);
+	}
+
+}
